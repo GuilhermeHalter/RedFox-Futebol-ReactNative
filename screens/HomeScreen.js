5c08022333478b91dfba557b94d226f86af570cc
GuilhermeHalter <guisam.halter@gmail.com>
2023-08-31 16:47:09 -0300

    "melhorando" o css

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,17 +1,24 @@
 import React from 'react';
-import LupaIcon from './img/pngwing.com.png'; 
+import LupaIcon from './img/pngwing.com.png';
+import { LinearGradient } from 'expo-linear-gradient';
 import { View, Text, StyleSheet, TextInput, Image, TouchableOpacity } from 'react-native';
 
 const HomeScreen = () => {
   return (
     <View style={styles.container}>
-      <View style={styles.menu}>
+       <LinearGradient
+        colors={['#ff0000', '#ff4d4d']} // Cores do gradiente
+        start={[0, 0]} // Ponto de início do gradiente
+        end={[1, 0]} // Ponto de fim do gradiente
+        style={styles.menu}
+        >
         <Text style={styles.menuItem}>Home</Text>
+        
         <View style={styles.searchBar}>
           <TextInput placeholder="Search" style={styles.searchInput} />
           <Image source={LupaIcon} style={styles.lupaIcon} />
         </View>
-      </View>
+        </LinearGradient>
       <View style={styles.content}>
         <Text style={styles.contentText}>Jogos da semana</Text>
         
@@ -19,7 +26,7 @@ const HomeScreen = () => {
           
           <View style={styles.column}>
             <Image source={require('./img/pngfind.com-bite-mark-png-631239.png')} style={styles.image} />
-            <Text style={styles.contentText}>VS</Text>
+            <Text style={styles.vsText}>VS</Text>
             <Image source={require('./img/pngfind.com-bite-mark-png-631239.png')} style={styles.image} />
             <TouchableOpacity style={styles.CompraButton}>
               <Text style={styles.CompraButtonText}>Comprar ingresso</Text>
@@ -29,7 +36,7 @@ const HomeScreen = () => {
           
           <View style={styles.column}>
             <Image source={require('./img/pngfind.com-bite-mark-png-631239.png')} style={styles.image} />
-            <Text style={styles.contentText}>VS</Text>
+            <Text style={styles.vsText}>VS</Text>
             <Image source={require('./img/pngfind.com-bite-mark-png-631239.png')} style={styles.image} />
             <TouchableOpacity style={styles.CompraButton}>
               <Text style={styles.CompraButtonText}>Comprar ingresso</Text>
@@ -38,7 +45,7 @@ const HomeScreen = () => {
 
           <View style={styles.column}>
             <Image source={require('./img/pngfind.com-bite-mark-png-631239.png')} style={styles.image} />
-            <Text style={styles.contentText}>VS</Text>
+            <Text style={styles.vsText}>VS</Text>
             <Image source={require('./img/pngfind.com-bite-mark-png-631239.png')} style={styles.image} />
             <TouchableOpacity style={styles.CompraButton}>
               <Text style={styles.CompraButtonText}>Comprar ingresso</Text>
@@ -53,80 +60,78 @@ const HomeScreen = () => {
 
 const styles = StyleSheet.create({
   container: {
-    display: 'flex',
+    flex: 1,
     backgroundColor: '#f2f2f2',
-    maxWidth:1000,
+    display:'flex',
+    
   },
   menu: {
-    justifyContent: 'space-around',
     flexDirection: 'row',
     alignItems: 'center',
     height: 60,
-    backgroundColor: '#3498db',
   },
   menuItem: {
     color: '#fff',
-    fontSize: 16,
+    fontSize: 18,
+    marginRight:10,
   },
   searchBar: {
+    flexDirection: 'row',
     backgroundColor: '#fff',
-    borderBottomWidth: 1,
+
     borderBottomColor: '#ccc',
+    borderRadius: 5,
+    flex: 1,
   },
   lupaIcon: {
     width: 20,
     height: 20,
-    marginRight: 10,
     marginTop:5, 
-    marginLeft:190,
-    position: 'absolute',
+    tintColor: '#555',
   },
   searchInput: {
-    height: 30, 
-    borderColor: '#ccc',
-    borderWidth: 1,
-    paddingRight: 30,
-    padding: 5, 
+    borderColor: 'transparent',
+    padding: 5,
+    flex: 1,
   },
   content: {
+    flex: 1,
     justifyContent: 'center',
-    display:'flex',
+    paddingHorizontal: 20,
   },
   contentText: {
-    fontSize: 20,
+    fontSize: 24,
     marginBottom: 20,
-    textAlign:'center',
-    marginTop:10,
-  },
-  column: {
-    display:'flex',
-    flexDirection:'row',
-    
+    textAlign: 'center',
+    color: '#555',
   },
   imageRow: {
     flexDirection: 'row',
-    justifyContent:'space-between',
-    
+    justifyContent: 'space-between',
+  },
+  column: {
+    alignItems: 'center',
   },
   image: {
-    width: 45,
-    height: 45,
-    marginBottom: 20,
-    
+    width: 100,
+    height: 100,
+    marginBottom: 10,
   },
-   CompraButton: {
-    display:'flex',
-    position:'absolute',
-    marginTop:70,
-    marginLeft:10,
+  vsText: {
+    fontSize: 18,
+    color: '#555',
+    marginVertical: 5,
+  },
+  CompraButton: {
     backgroundColor: '#27ae60',
     borderRadius: 5,
     paddingVertical: 10,
-    paddingHorizontal: 10,
+    paddingHorizontal: 20,
+    marginTop: 10,
   },
   CompraButtonText: {
     color: '#fff',
-    fontSize: 10,
+    fontSize: 16,
     textAlign: 'center',
   },
 });
